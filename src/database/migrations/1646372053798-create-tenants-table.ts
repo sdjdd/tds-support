3a8b1d1f2713f46461f6eed8772158fb0cc46a65
sdjdd <i@sdjdd.com>
2022-03-04 17:41:48 +0800

    feat: database level defaul value

--- src/database/migrations/1646372053798-create-tenants-table.ts
+++ src/database/migrations/1646372053798-create-tenants-table.ts
@@ -7,7 +7,7 @@ export class createTenantsTable1646372053798 implements MigrationInterface {
         id          int(11) unsigned NOT NULL AUTO_INCREMENT,
         subdomain   varchar(191)     NOT NULL,
         name        varchar(255)     NOT NULL,
-        description varchar(255)     NOT NULL,
+        description varchar(255)     NOT NULL DEFAULT '',
         created_at  datetime(3)      NOT NULL DEFAULT NOW(3),
         updated_at  datetime(3)      NOT NULL DEFAULT NOW(3) ON UPDATE NOW(3),
         PRIMARY KEY (id),
