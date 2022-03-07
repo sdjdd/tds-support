c2e12ca85ea073d5711cfa3e1d34fae4f613202d
sdjdd <i@sdjdd.com>
2022-03-07 11:47:12 +0800

    fix: add missing provicers

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
+import { Subdomain } from './entities/subdomain.entity';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
+import { SubdomainsService } from './subdomains.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
-  providers: [ProjectsService],
+  imports: [TypeOrmModule.forFeature([Project, Subdomain])],
+  providers: [ProjectsService, SubdomainsService],
   controllers: [ProjectsController],
 })
 export class ProjectsModule {}
