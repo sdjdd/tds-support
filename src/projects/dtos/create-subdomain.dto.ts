af24800e737ace5f607fcbfc62a5987e3765406b
sdjdd <i@sdjdd.com>
2022-03-07 11:56:07 +0800

    refactor: regex of subdomain

--- src/projects/dtos/create-subdomain.dto.ts
+++ src/projects/dtos/create-subdomain.dto.ts
@@ -1,7 +1,11 @@
 import { IsString, Matches } from 'class-validator';
 
 export class CreateSubdomainDto {
-  @Matches(/^[a-z0-9_-]+$/i)
+  // Ref: https://stackoverflow.com/questions/7930751/regexp-for-subdomain
+  // But no capitals
+  @Matches(/^[a-z0-9](?:[a-z0-9\-]{0,61}[a-z0-9])?$/, {
+    message: 'invalid subdomain',
+  })
   @IsString()
   subdomain: string;
 }
