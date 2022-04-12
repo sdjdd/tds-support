f40febffd697da4dbd902e306caa0d6e7cb38fd5
sdjdd <i@sdjdd.com>
2022-04-12 16:20:29 +0800

    feat: update category position

--- src/category/dtos/update-category-dto.ts
+++ src/category/dtos/update-category-dto.ts
@@ -5,6 +5,7 @@ import { CreateCategorySchema } from './create-category.dto';
 export const UpdateCategorySchema = CreateCategorySchema.partial().extend({
   parentId: CreateCategorySchema.shape.parentId.nullable(),
   active: z.boolean().optional(),
+  position: z.number().int().optional(),
 });
 
 export class UpdateCategoryDto extends createZodDto(UpdateCategorySchema) {}
